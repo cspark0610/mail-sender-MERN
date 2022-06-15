aa7bccb6ef2fc16555e8e51b63b31b68ca955c06
cspark0610 <cpark0610@gmail.com>
2022-06-15 15:48:52 -0300

    2nd change

--- config/keys.js
+++ config/keys.js
@@ -6,10 +6,3 @@ if (process.env.NODE_ENV === "production") {
 	//in development environment
 	module.exports = require("./dev");
 }
-
-// module.exports = {
-// 	googleClientID: "304984529930-i17hsqst2fmkjgdtshlv3qtcpndie5e8.apps.googleusercontent.com",
-// 	googleClientSecret: "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",
-// 	mongoURI: "mongodb+srv://carlos:Plataforma5@cluster0.shsdz.mongodb.net/?retryWrites=true&w=majority",
-// 	cookieKey: "123123123",
-// };
